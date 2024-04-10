143bf8c897e0e1e2b695877c3ca86655cc25218e
Guilherme Oliveira <50001170+GuilhermeeOliveiraa@users.noreply.github.com>
2024-04-10 17:23:05 -0300

    Update sistema.py

--- sistema.py
+++ sistema.py
@@ -11,22 +11,54 @@ saldo = 0
 limite = 500
 extrato = ""
 numero_saques = 0 
-LIMITE_SAQUES = 3
+LIMITE_DE_SAQUES = 3
+LIMITE_POR_SAQUE = 500
 
 while True:
 
     opcao = input(menu)
 
-    if opcao == d:
-        print("Depósito")
+    if opcao == "d":
+        deposito = float(input("Insira quanto deseja depositar em R$: "))
 
-    elif opcao == s:
-        print("Saque")
+        if deposito > 0:
+            saldo += deposito
+            print(f"Depósito realizado com sucesso.")
+            extrato += f"Deposito de R$ {deposito:.2f} \n"
 
-    elif opcao == e:
-        print("Extrato")
+        else:
+            print("Valor inválido.")
 
-    elif opcao == q:
+    elif opcao == "s":
+
+        if numero_saques < LIMITE_DE_SAQUES:
+            saque = float(input("Insira quanto deseja sacar em R$: "))
+
+            if saque > saldo:
+                print("Você não possui saldo suficiente.")
+
+            elif saque > LIMITE_POR_SAQUE:
+                print("Limite de R$ 500.00 por saque.")
+
+            elif saque > 0:
+                saldo -= saque
+                print(f"Saque realizado com sucesso.")
+                numero_saques += 1
+                extrato += f"Saque de R$ {saque:.2f} \n"
+
+            else:
+               print("Valor inválido.")
+
+        else:
+            print('Limite de saques diários atingido.')
+
+    elif opcao == "e":
+        print("============== Extrato ==============")
+        print("Não foram realizadas operações" if not extrato else extrato)
+        print(f"\nVocê possui atualmente R$ {saldo:.2f}")
+        print("=====================================")
+
+    elif opcao == "q":
         break
 
     else:
